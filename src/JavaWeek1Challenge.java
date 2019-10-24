01d4e3aee0646ecc33b9afef80f963cb69c2bbbd
Ksenia Lake <ksenialake@gmail.com>
2019-10-24 10:55:24 -0400

    part 2

--- src/JavaWeek1Challenge.java
+++ src/JavaWeek1Challenge.java
@@ -51,23 +51,46 @@ public class JavaWeek1Challenge {
             System.out.println("Please enter a valid input.");
         }
 
-//        /* *****************************
-//         *           PART 2
-//         * ****************************/
-//        // declare + initialize :: part 2
-//        Random randomGenerator = new Random();
-//        int start = 1, int end = 250;
-//        int randomInt;
-//
-//        // required outputs:
-//        System.out.println(randomInt + " is not a prime number.");
-//        // or:
-//        System.out.println(randomInt + " is a prime numbwer!");
+        /* *****************************
+         *           PART 2
+         * ****************************/
+        // declare + initialize :: part 2
+        Random randomGenerator = new Random();
+        int start = 1, end = 250;  // set RANGE
+        int randomInt;
+
+        // greet the user
+        System.out.println("The program will now generate a random number between " + start + " and " + end + " and check if it is prime.");
+
+        // generate a random number in RANGE
+        randomInt = randomGenerator.nextInt((end - start) + 1) + start;
+
+        if (randomInt == 2) { // TWO is a SPECIAL CASE!
+            System.out.println(randomInt + " is prime!");
+        } else { // userInt > 2; CHECK IF PRIME
+
+            isPrime = true;   // set flag to true -- our loop will check for false condition
+
+            for (int i = 2; i <= (randomInt / 2); i++) {  // only check up to userInt/2 (integer division is okay)
+                if (randomInt % i == 0) {
+                    System.out.println(randomInt + " is not a prime number.");
+                    isPrime = false;
+                    break;
+                }
+            } // end prime-checking loop
+
+            // if we have gotten this far and the flag is still true, no number could divide userInt, it is Prime.
+            if (isPrime) {
+                System.out.println(randomInt + " is prime!");
+            }
+
+        } // end 2-not2 if-else
+
 
         /* *****************************
          *       Clean-Up and Exit
          * ****************************/
-        System.out.println("Thanks for playing!");
+//        System.out.println("Thanks for playing!");
         keyboard.close();
 
     }  // end main
