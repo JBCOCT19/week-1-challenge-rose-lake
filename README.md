4fe8cc0f0d40beb378df4dd432d65dbf0fd9252d
Ksenia Lake <ksenialake@gmail.com>
2019-10-24 11:41:18 -0400

    requirements complete

--- README.md
+++ README.md
@@ -20,3 +20,38 @@ After part 1, in the next part generate a random number between 1 and 250 (you c
 
 As with part 1, you will print the number and print whether it is a prime number. 
 
+### wrapper 1:
+
+Modify your program to give the user a choice to pick between part 1 and part 2 at the start of the program.
+
+For example, the program should ask the user:
+```
+"Would you like to generate a random number or input a number? (R/I)"
+
+// if the user types "R", proceed with random number
+
+"Generating a random number..."
+
+// otherwise, if the user types "I", proceed with taking an input
+
+"Please enter a number: "
+```
+
+### wrapper 2:
+
+Modify your program  now to give the user a choice to try again.
+ 
+```
+"Would you like to try again? (Y/N)"
+
+// if the user types "Y", proceed with repeating the program
+"Would you like to generate a random number or input a number? (R/I)"
+
+// if the user types "R", proceed with random number
+
+"Generating a random number..."
+
+// otherwise, if the user types "I", proceed with taking an input
+
+"Please enter a number: "
+```
--- src/JavaWeek1Challenge.java
+++ src/JavaWeek1Challenge.java
@@ -10,87 +10,139 @@ import java.util.Random;
 public class JavaWeek1Challenge {
     public static void main(String[] args) {
 
-        /* *****************************
-         *            PART 1
-         * ****************************/
-        // declare + initialize :: part 1 (and some also for part 2)
-        Scanner keyboard = new Scanner(System.in);
-        int userInt;
-        boolean isPrime;
+        /* *************************
+         *  variable declarations
+         * ************************/
+        Scanner keyboard = new Scanner(System.in);      // keyboard scanner used throughout
+        boolean isPrime;                                // prime-checking control flag
 
-        // request user input
-        System.out.println("Hello! Welcome to the prime number game!\nPlease enter an integer greater than 1.");
-        userInt = keyboard.nextInt();
-        keyboard.nextLine();
+        int userInt;                                    // user inputted number
+        String userChoice;                              // holds user responses
 
-        if (userInt > 1) {  // validate input
+        Random randomGenerator = new Random();          // random number generator
+        int start = 1, end = 250;                       // RANGE for random number generator
+        int randomInt;                                  // randomly generated integer
 
-            if (userInt == 2) { // TWO is a SPECIAL CASE!
-                System.out.println(userInt + " is prime!");
-            }
-            else { // userInt > 2; CHECK IF PRIME
+        boolean playAgain = false;                      // control flag to play game again
+        boolean validChoice;                            // control flag (validate user response)
 
-                isPrime = true;   // set flag to true -- our loop will check for false condition
 
-                for (int i = 2; i <= (userInt / 2); i++) {  // only check up to userInt/2 (integer division is okay)
-                    if (userInt % i == 0){
-                        System.out.println(userInt + " is not a prime number.");
-                        isPrime = false;
-                        break;
+        /* *************************************************
+         *  wrapper 2: give option to play again : do-while
+         * *************************************************/
+        do {
+            /* **********************************************
+             *  wrapper 1: ask user to choose part 1 or part 2
+             * *********************************************/
+            System.out.println("Hello! Welcome to the prime number game!");
+            System.out.println("Would you like to input your own number (\"I\") or have the program generate a random number (\"R\")?" );
+
+            userChoice = keyboard.nextLine();
+
+            if(userChoice.equalsIgnoreCase("i")) {
+
+                /* *****************************
+                 *            PART 1
+                 * ****************************/
+                // request user input
+                System.out.println("Please enter an integer greater than 1.");
+                userInt = keyboard.nextInt();
+                keyboard.nextLine();
+
+                if (userInt > 1) {  // validate input
+
+                    if (userInt == 2) { // TWO is a SPECIAL CASE!
+                        System.out.println(userInt + " is prime!");
                     }
-                } // end prime-checking loop
+                    else { // userInt > 2; CHECK IF PRIME
 
-                // if we have gotten this far and the flag is still true, no number could divide userInt, it is Prime.
-                if (isPrime) {
-                    System.out.println(userInt + " is prime!");
+                        isPrime = true;   // set flag to true -- our loop will check for false condition
+
+                        for (int i = 2; i <= (userInt / 2); i++) {  // only check up to userInt/2 (integer division is okay)
+                            if (userInt % i == 0){
+                                System.out.println(userInt + " is not a prime number.");
+                                isPrime = false;
+                                break;
+                            }
+                        } // end prime-checking loop
+
+                        // if we have gotten this far and the flag is still true, no number could divide userInt, it is Prime.
+                        if (isPrime) {
+                            System.out.println(userInt + " is prime!");
+                        }
+
+                    } // end 2-not2 if-else
+
+                } else { // user input was not an integer > 1
+                    System.out.println("That was not a valid input.");
                 }
 
-            } // end 2-not2 if-else
 
-        } else { // user input was not an integer > 1
-            System.out.println("Please enter a valid input.");
-        }
+            } else if(userChoice.equalsIgnoreCase("r")) {
 
-        /* *****************************
-         *           PART 2
-         * ****************************/
-        // declare + initialize :: part 2
-        Random randomGenerator = new Random();
-        int start = 1, end = 250;  // set RANGE
-        int randomInt;
+                /* *****************************
+                 *           PART 2
+                 * ****************************/
+                // greet the user
+                System.out.println("The program will now generate a random number between " + start + " and " + end + " and check if it is prime.");
 
-        // greet the user
-        System.out.println("The program will now generate a random number between " + start + " and " + end + " and check if it is prime.");
+                // generate a random number in RANGE
+                randomInt = randomGenerator.nextInt((end - start) + 1) + start;
 
-        // generate a random number in RANGE
-        randomInt = randomGenerator.nextInt((end - start) + 1) + start;
+                if (randomInt == 2) { // TWO is a SPECIAL CASE!
+                    System.out.println(randomInt + " is prime!");
+                } else { // userInt > 2; CHECK IF PRIME
 
-        if (randomInt == 2) { // TWO is a SPECIAL CASE!
-            System.out.println(randomInt + " is prime!");
-        } else { // userInt > 2; CHECK IF PRIME
+                    isPrime = true;   // set flag to true -- our loop will check for false condition
 
-            isPrime = true;   // set flag to true -- our loop will check for false condition
+                    for (int i = 2; i <= (randomInt / 2); i++) {  // only check up to userInt/2 (integer division is okay)
+                        if (randomInt % i == 0) {
+                            System.out.println(randomInt + " is not a prime number.");
+                            isPrime = false;
+                            break;
+                        }
+                    } // end prime-checking loop
 
-            for (int i = 2; i <= (randomInt / 2); i++) {  // only check up to userInt/2 (integer division is okay)
-                if (randomInt % i == 0) {
-                    System.out.println(randomInt + " is not a prime number.");
-                    isPrime = false;
-                    break;
-                }
-            } // end prime-checking loop
+                    // if we have gotten this far and the flag is still true, no number could divide userInt, it is Prime.
+                    if (isPrime) {
+                        System.out.println(randomInt + " is prime!");
+                    }
 
-            // if we have gotten this far and the flag is still true, no number could divide userInt, it is Prime.
-            if (isPrime) {
-                System.out.println(randomInt + " is prime!");
+                } // end 2-not2 if-else
+
+            } else {
+                System.out.println("That was not a valid choice.");
             }
 
-        } // end 2-not2 if-else
 
+            /* **********************************************************
+             *  ask user if they want to play again
+             *  validate their answer to be ignorecase(y | yes | n | no)
+             *  keep asking until valid response
+             * **********************************************************/
+            validChoice = false;
+            while(!validChoice) {
+                System.out.println("Would you like to play again? (Y/N) ");
+                userChoice = keyboard.nextLine();
+                if (userChoice.equalsIgnoreCase("y") || userChoice.equalsIgnoreCase("yes")) {
+                    playAgain = true;
+                    validChoice = true;
+                }
+                else if (userChoice.equalsIgnoreCase("n") || userChoice.equalsIgnoreCase("no")) {
+                    playAgain = false;
+                    validChoice = true;
+                }
+                else {
+                    System.out.println("Please enter a valid choice.");
+                }
+            }
+
+        } while (playAgain);
 
         /* *****************************
          *       Clean-Up and Exit
          * ****************************/
-//        System.out.println("Thanks for playing!");
+        System.out.println("Thanks for playing!");
         keyboard.close();
 
     }  // end main
